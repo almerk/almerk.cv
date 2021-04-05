80cc6b4123ad39477f361cf48b45df0638f4bac3
almerk <almerk.work@gmail.com>
2021-04-05 16:40:03 +0300

    Added missing sections to js file

--- src/info.js
+++ src/info.js
@@ -4,7 +4,7 @@ export default {
         career: "Fullstack software developer",
         qualification: { label: "Qualification", value: "Middle" },
         name: "Aleksey Merkuliev",
-        age: "26 years",
+        age: "26 y.o.",
         place: "Moscow, Russia",
         sex: "Male",
         contacts: {
@@ -13,13 +13,72 @@ export default {
                 label: "📧almerk.work@gmail.com",
                 href: "mailto:almerk.work@gmail.com",
             },
-            tlg: { label: "@Almerk_tlg", href: "tg://resolve?domain=Almerk_tlg" },
+            tlg: { label: "Tg: @Almerk_tlg", href: "tg://resolve?domain=Almerk_tlg" },
             skype: {
-                label: "Skype",
+                label: "Skype: Aleksey Merkuliev",
                 href: "https://join.skype.com/invite/lqjQveWJBf5Q",
             },
-            github: { label: "Github", href: "https://github.com/almerk" },
+            github: { label: "Github: almerk", href: "https://github.com/almerk" },
         },
+        desiredJob: {
+            label: "Desired position and responsibilities",
+            values: [{ " Position": "Middle Software developer " },
+            {
+                "Responsibilities": ["Backend development using modern .NET features (ASP.NET core, LINQ, Entity framework, Docker integration)",
+                    "Frontend development using popular javascript frameworks (Vue, React)"]
+            }, "Ready for remote work", "ready for business trips only in Russia"]
+        },
+        professionalSkills: {
+            label: "Professional skills",
+            values: [
+                { "General": ["Web applications development", "Desktop app development for OS Windows, using WPF", "Writing code according to generally accepted patterns"] },
+                { "Languages": ["C#, Javascript, SQL, HTML/CSS, Python"] },
+                { "Technologies, platforms and frameworks": [".NET", "ASP.NET", "WPF", "Vue.js", "ASP.NET core", "Node.js + Express", "Docker"] },
+                { "DBMS": ["Oracle", "MsSQL", "MongoDB", "SQLite"] }
+            ]
+        },
+        experience: {
+            label: "Experience",
+            values: {
+                place: "Public service",
+                time: "July 2016 - June 2021 (5 years)",
+                post: "Software engineer",
+                responsibilities: [
+                    "Web applications development and support, written on ASP.NET",
+                    "Modernizing legacy web applications, written on ASP.NET Webforms",
+                    "Desktop app development for OS Windows, using WPF",
+                    "Process automation using lightweight .NET applications and python scripts",
+                    "Initiation and participation in microservice migration process",
+                    "Deployment of CI/CD infrastucture using Gitlab tools"
+                ],
+            }
+        },
+        education: {
+            label: "Education",
+            values: [
+                {
+                    time: "2011-2016 (5 years)",
+                    place: "Institute of Communication Cryptography and Informatics, FSB Academy",
+                    faculty: "Information security",
+                    degree: "Specialist"
+                }
+            ],
+        },
+        knownLanguages: {
+            label: "Known languages",
+            values: [
+                { "Russian": "Native" },
+                { "English": "B2 (upper intermediate)" }
+            ]
+        },
+        personalSkills: {
+            label: "Personal skills",
+            values: ["Responsibility", "Desire to grow as a professional", "Sense of humor"]
+        },
+        aboutMe: {
+            label: "About me",
+            values: "I am not only changing my current place of work, but also changing my lifestyle and the way I work. In the last 2 years, I have been actively engaged in updating my own knowledge and applying it at work. Due to the contractual nature of the current work, I will be able to start new not earlier than 22/06/2021"
+        }
     },
     "ru": {
         pdf: { label: "Скачать в PDF" },
@@ -35,12 +94,71 @@ export default {
                 label: "📧almerk.work@gmail.com",
                 href: "mailto:almerk.work@gmail.com",
             },
-            tlg: { label: "@Almerk_tlg", href: "tg://resolve?domain=Almerk_tlg" },
+            tlg: { label: "Tg: @Almerk_tlg", href: "tg://resolve?domain=Almerk_tlg" },
             skype: {
-                label: "Skype",
+                label: "Skype: Aleksey Merkuliev",
                 href: "https://join.skype.com/invite/lqjQveWJBf5Q",
             },
-            github: { label: "Github", href: "https://github.com/almerk" },
+            github: { label: "Github: almerk", href: "https://github.com/almerk" },
         },
+        desiredJob: {
+            label: "Desired position and responsibilities",
+            values: [{ " Position": "Middle Software developer " },
+            {
+                "Responsibilities": ["Backend development using modern .NET features (ASP.NET core, LINQ, Entity framework, Docker integration)",
+                    "Frontend development using popular javascript frameworks (Vue, React)"]
+            }, "Ready for remote work", "ready for business trips only in Russia"]
+        },
+        professionalSkills: {
+            label: "Professional skills",
+            values: [
+                { "General": ["Web applications development", "Desktop app development for OS Windows, using WPF", "Writing code according to generally accepted patterns"] },
+                { "Languages": ["C#, Javascript, SQL, HTML/CSS, Python"] },
+                { "Technologies, platforms and frameworks": [".NET", "ASP.NET", "WPF", "Vue.js", "ASP.NET core", "Node.js + Express", "Docker"] },
+                { "DBMS": ["Oracle", "MsSQL", "MongoDB", "SQLite"] }
+            ]
+        },
+        experience: {
+            label: "Experience",
+            values: {
+                place: "Public service",
+                time: "July 2016 - June 2021 (5 years)",
+                post: "Software engineer",
+                responsibilities: [
+                    "Web applications development and support, written on ASP.NET",
+                    "Modernizing legacy web applications, written on ASP.NET Webforms",
+                    "Desktop app development for OS Windows, using WPF",
+                    "Process automation using lightweight .NET applications and python scripts",
+                    "Initiation and participation in microservice migration process",
+                    "Deployment of CI/CD infrastucture using Gitlab tools"
+                ],
+            }
+        },
+        education: {
+            label: "Education",
+            values: [
+                {
+                    time: "2011-2016 (5 years)",
+                    place: "Institute of Communication Cryptography and Informatics, FSB Academy",
+                    faculty: "Information security",
+                    degree: "Specialist"
+                }
+            ],
+        },
+        knownLanguages: {
+            label: "Known languages",
+            values: [
+                { "Russian": "Native" },
+                { "English": "B2 (upper intermediate)" }
+            ]
+        },
+        personalSkills: {
+            label: "Personal skills",
+            values: ["Responsibility", "Desire to grow as a professional", "Sense of humor"]
+        },
+        aboutMe: {
+            label: "About me",
+            values: "I am not only changing my current place of work, but also changing my lifestyle and the way I work. In the last 2 years, I have been actively engaged in updating my own knowledge and applying it at work. Due to the contractual nature of the current work, I will be able to start new not earlier than 22/06/2021"
+        }
     }
 }
